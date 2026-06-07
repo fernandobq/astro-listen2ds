e67de8c896f66b8f60fb4eebc0ee0d73c94e5e6b
Fernando Barraza Quintero <febaqui1008@gmail.com>
2026-06-06 18:13:36 -0700

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [
+    sanity({
+      projectId: "4fqkkmt0",
+      dataset: "production",
+      useCdn: false, // for static builds
+    }),
+  ],
 });
